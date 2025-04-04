023f0f403e23d9659b7dafb2d4c9a3af0c16c535
Benedek Kincses <benedek.kincses@gmail.com>
2025-04-04 16:25:22 +0200

    Init

---
+++ backup-restore/backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check if container name is provided
+if [ -z "$1" ]; then
+	echo "Container name required"
+	exit 1
+fi
+
+echo "Starting..."
+# Define variables
+container=$1
+timestamp=$(date "+%Y_%m_%d_%H%M")
+filename="backup_${timestamp}.tar"
+
+printf "COMPRESSING WORLD DIRECTORIES..."
+# Create tar file in container
+docker exec ${container} tar -cf ${filename} world world_nether world_the_end
+echo "DONE!"
+
+printf "COPYING TO HOST..."
+# Copy tar file to host
+cd backup
+docker cp ${container}:/data/${filename} ${filename} > /dev/null
+cd ..
+echo "DONE!"
+
+printf "CLEANING UP..."
+# Cleanup
+docker exec ${container} rm ${filename}
+echo "DONE!"
+
+echo "BACKUP OPERATION SUCCESSFULL!"
---
+++ backup-restore/restore.sh
@@ -0,0 +1,31 @@
+# Check if argument is provided
+if [ -z "$1" ]; then
+	echo "Tar file required"
+	exit 1
+fi
+if [ -z "$2" ]; then
+	echo "Container name required"
+	exit 1
+fi
+
+# Store argument
+filename=$(basename -- "$1")
+container=$2
+
+# Remove world directories from container
+printf "REMOVING PREVIOUS WORLDS..."
+docker exec ${container} rm -r world world_nether world_the_end
+echo "DONE!"
+
+# Copy and extract tar file
+printf "COPYING AND EXTRACTING FILE..."
+docker cp ${filename} ${container}:/data/
+docker exec ${container} tar -xf ${filename}
+echo "DONE!"
+
+# Cleanup
+printf "CLEANING UP..."
+docker exec ${container} rm ${filename}
+echo "DONE!"
+
+echo "RESTORE OPERATION SUCCESSFULL!"
